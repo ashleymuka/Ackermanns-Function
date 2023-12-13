716c586850dbc77155fb170571b4f8ba46e82ebf
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 23:29:16 -0600

    Update Ackermann's function.py

--- Ackermann's function.py
+++ Ackermann's function.py
@@ -1,17 +1,9 @@
 
 '''
-Author: Ashley Muka
-Assignment Title: Ackermann's Function
-Assignment Description:implement a Python function ackermann(m,n)
+Description:implement a Python function ackermann(m,n)
 that calculates Ackermann's function. 
-Due Date: 11/03/2023
-Date Created:11/01/2023
-Date Last Modified:11/01/2023
-
 '''
 
-
-#process
 def ackermann(m, n):
     if m == 0:
         return n + 1
@@ -23,12 +15,12 @@ def ackermann(m, n):
         return ackermann(m-1, ackermann(m, n-1))
 
 if __name__ == "__main__":
-#input
+
     mValues = [0,1,2,3]
     nValues = [0,1,2,3]
 
     for m in mValues:
         for n in nValues:
             result = ackermann(m, n)
-#output           
+     
             print(f' Ackermann({m}, {n}) = {result}')
